3dabd2a63a99d1220be11c471b16cdc7838abe09
arnavvjainn <95885292+arnavvjainn@users.noreply.github.com>
2021-12-15 09:51:43 -0500

    Added item classes

---
+++ item.h
@@ -0,0 +1,16 @@
+#ifndef _ITEM_H_ 
+#define _ITEM_H_
+
+#include <utility> 
+
+using namespace std;
+
+class Item {
+    double buff;
+    pair<int, int> ItemPos;
+    public:
+    Item(double buff);
+    double getBuff();
+    pair<int, int> getItemPos(); 
+    ~Item();
+}
